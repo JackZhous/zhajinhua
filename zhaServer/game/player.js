046dcead1ecb85068ea8076326692785eb481ecc
Jack_win32 <jackzhouyu@foxmail.com>
2017-10-09 23:20:33 +0800

    添加服务端用户下线功能

--- zhaServer/game/player.js
+++ zhaServer/game/player.js
@@ -4,17 +4,22 @@ const Player = function (uid, socket, event) {
   var _socket = socket;
   var _event = event;
 
+
+  //监听玩家掉线
+  _socket.on("disconnect", function () {
+    console.log("玩家掉线，uid="+_uid);
+    _event.fire("disconnect", _uid);
+  });
     /**
      * 同步数据,表示我已经登录了
      * @param data
      */
   that.sendSyncData = function (data) {
     console.log("syncData " + JSON.stringify(data));
-    socket.emit("sync_data", data);
+    _socket.emit("sync_data", data);
     var length = data.playerList.length - 1;
 
     for(let i = 0; i < length; i++){
-
       sendCreatePlayerMsg({uid:data.playerList[i].getUid(), index:i});
     }
   };
@@ -23,6 +28,7 @@ const Player = function (uid, socket, event) {
     return _uid;
   };
 
+  //玩家加入消息
   const sendCreatePlayerMsg = function (data) {
     console.log("send create player msg" + JSON.stringify(data));
     if(data.uid != _uid){
@@ -30,10 +36,17 @@ const Player = function (uid, socket, event) {
     }
   };
 
+  //玩家下线消息
+  const sendOfflinePlayerMsg = function (uid) {
+      _socket.emit("player_offline", uid);
+  };
+
   //多个player都调用这个方法则都在event都会保存多个注册者
   _event.on("sendPlayerMessage", sendCreatePlayerMsg);
+  _event.on("sendOfflinePlayerMsg", sendOfflinePlayerMsg);
 
   that.destroy = function () {
+    _event.off("sendOfflinePlayerMsg", sendOfflinePlayerMsg)
     _event.off("sendPlayerMessage", sendCreatePlayerMsg);
   };
   return that;
--- zhaServer/game/room.js
+++ zhaServer/game/room.js
@@ -16,9 +16,19 @@ const  Room = function () {
         plater.sendSyncData({uid:uid, index: _playerList.length - 1, playerList:_playerList});
 
         _event.fire("sendPlayerMessage", {uid:uid, index:_playerList.length - 1});
-
-
     };
+
+    _event.on("disconnect", function (uid) {
+       for(let i = 0, i < _playerList.length; i++){
+           // noinspection JSAnnotator
+            if(uid == _playerList[i].getUid()){
+                _playerList[i].destroy();
+                _playerList.splice(i, 1);
+           }
+        }
+        //这里有可能会对所有的用户都发送离线消息，这样是不对的，应该只对同一个房间的用户发送才对的
+        _event.fires("sendOfflinePlayerMsg", uid);
+    });
     return that;
 };
 
